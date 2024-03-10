9ed33163d28cc7417f39c7f56c67ba2f1c12b198
MinhT <phammanhtoanhht933@gmail.com>
2024-03-11 02:53:32 +0700

    refactor all code authentication, authorization, relationship database

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { AuthService } from '@apis/auth/auth.service';
 import { LoginDto } from '@apis/auth/dto/login.dto';
 import { Public } from '@apis/auth/contants';
 import { Response } from 'express';
 import { ApiCookieAuth, ApiTags } from '@nestjs/swagger';
 import { SignUpDTO } from '@apis/user/dto/user.dto';
+import { User } from '@app/decocrator/user.decorator';
 
 @Controller('auth')
 @ApiCookieAuth()
@@ -18,9 +19,14 @@ export class AuthController {
     const { access_token } = await this.authService.login(body);
     response
       .cookie('access_token', access_token, {
-        httpOnly: false, //cross-site scripting (XSS) attacks
-        secure: false, //sent over HTTPS connections
-        sameSite: 'lax',
+        path:'/',
+        // domain:'http://127.0.0.1',
+        // tìm hiếu thêm
+        httpOnly: true, //cross-site scripting (XSS) attacks
+        // tìm hiểu thêm
+        secure:true , //sent over HTTPS connections
+        // tìm hiểu thêm
+        sameSite: 'none',
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       })
       .send({ status: 'ok' });
@@ -35,5 +41,9 @@ export class AuthController {
   @Post('logout')
   async logout(@Res({ passthrough: true }) response: Response) {
     response.cookie('access_token', '').send({ message: 'logout success' });
+  }
+  @Get('me')
+  async getMe(@User() user) {
+    return JSON.parse(user);
   }
 }
--- src/apis/role/role.controller.ts
+++ src/apis/role/role.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { RoleService } from '@apis/role/role.service';
+import { RoleDto } from '@apis/role/dto/role.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Public } from '@apis/auth/contants';
 
 @Controller('role')
+@ApiTags('role')
 export class RoleController {
   constructor(private roleService: RoleService) {}
+  @Public()
+  @Post('role')
+  async createRole(@Body() body:RoleDto){
+    return this.roleService.create(body);
+  }
 }
