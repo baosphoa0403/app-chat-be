0c93fb35b261074df616906ce53e48cc2df99f7c
Gia Bảo <baosphoa0403@gmail.com>
2024-04-21 00:58:20 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { AppService } from '@app/app.service';
 import { DatabaseModule } from '@modules/database/database.module';
 import { ConfigModule } from '@modules/config/config.module';
 import { JwtModuleShared } from '@modules/share/jwt/jwt.module';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@apis/auth/auth.guard';
-import { RolesGuard } from '@app/guards/role.guard';
+import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from '@app/fillter/http-exception.filter';
 import { ApiModule } from '@apis/api.module';
 
@@ -15,14 +13,14 @@ import { ApiModule } from '@apis/api.module';
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard
-    },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: AuthGuard
+    // },
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: RolesGuard
+    // },
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter
